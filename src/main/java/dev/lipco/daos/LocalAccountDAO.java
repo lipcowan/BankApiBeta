4d1fa58eea4a4ee574274b3593c5b6655f275fa0
lip <phillip@lipco.dev>
2021-02-18 09:12:16 -0700

    working on local account dao and service

--- src/main/java/dev/lipco/daos/LocalAccountDAO.java
+++ src/main/java/dev/lipco/daos/LocalAccountDAO.java
@@ -1,5 +1,43 @@
 package dev.lipco.daos;
 
+import dev.lipco.entities.Account;
 
-public class LocalAccountDAO {
+import java.util.*;
+
+public class LocalAccountDAO implements AccountDAO {
+
+    private final Map<Integer, Account> account_table = new HashMap<Integer, Account>();
+    private int idCounter = 0;
+
+    @Override
+    public Account createAccount(Account account) {
+        account.setAccountId(++ this.idCounter);
+        account_table.put(this.idCounter, account);
+        return account;
+    }
+
+    @Override
+    public Set<Account> getAllAccounts() {
+        Set<Account> accounts = new HashSet<Account>(this.account_table.values());
+        return accounts;
+    }
+
+    @Override
+    public Account getAccountById(int accountId) {
+        Account account = this.account_table.get(accountId);
+        return account;
+    }
+
+    @Override
+    public Account updateAccount(Account account) {
+        this.account_table.put(account.getAccountId(), account);
+        return account;
+    }
+    
+    @Override
+    public boolean deleteAccountById(int accountId) {
+        Account account = account_table.remove(accountId);
+        return account != null;
+    }
+
 }
